90919576e9d71b3f2a960f74b2bd168bf15afe77
Helena Oliveira <helena@informach.com.br>
2022-06-23 07:00:17 -0300

    add iframe to execute code

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
@@ -38,15 +39,30 @@ const App = () => {
       },
     });
 
-    setCode(result.outputFiles[0].text);
-
-    try {
-      eval(result.outputFiles[0].text);
-    } catch (err) {
-      alert(err);
-    }
+    // setCode(result.outputFiles[0].text);
     
-  }
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+  };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -55,6 +71,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html} />
     </div>
   )
 };
