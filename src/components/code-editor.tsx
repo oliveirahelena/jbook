1b648f031c1cc896492b52d590d1f30ee3ed6dbd
Helena Oliveira <helena@informach.com.br>
2022-06-24 05:51:12 -0300

    add settings to monaco editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,13 +1,34 @@
-import MonacoEditor from '@monaco-editor/react'
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
 
-const CodeEditor = () => {
+interface CodeEditorProps {
+    initialValue: string;
+    onChange(value: string): void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+    const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+        monacoEditor.onDidChangeModelContent(() => {
+            onChange(getValue());
+        });
+
+        monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+    }
     return (
-        <MonacoEditor 
+        <MonacoEditor
+            editorDidMount={onEditorDidMount}
+            value={initialValue}
             theme="dark" 
             language='javascript' 
             height="500px" 
             options={{
-                wordWrap: 'on'
+                wordWrap: 'on',
+                minimap: { enabled: false },
+                showUnused: false,
+                folding: false,
+                lineNumbersMinChars: 3,
+                fontSize: 16,
+                scrollBeyondLastLine: false,
+                automaticLayout: true,
             }}
         />
     )
