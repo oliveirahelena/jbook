d6f2c74e490b4599f195e70352ee776e4fda8b03
Helena Oliveira <helena@informach.com.br>
2022-06-26 09:34:43 -0300

    add bundle to global state

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import CodeEditor from './code-editor';
 import Preview from './preview';
-import bundle from '../bundler';
 import Resizable from './resizable';
 import { Cell } from '../state';
 import { useActions } from '../hooks/use-actions';
+import { useTypedSelector } from '../hooks/use-typed-selector';
 
 
 interface CodeCellProps {
@@ -12,21 +12,23 @@ interface CodeCellProps {
 }
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
-  const [code, setCode] = useState('');
-  const [err, setErr] = useState('');
-  const { updateCell } = useActions();
+  const { updateCell, createBundle } = useActions();
+  const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
+    
     const timer = setTimeout(async () => {
-      const output = await bundle(cell.content);
-      setCode(output.code);
-      setErr(output.err);
-    }, 1000);
+      createBundle(cell.id, cell.content);
+    }, 750);
 
     return () => {
       clearTimeout(timer);
     }
-  }, [cell.content]);
+  }, [cell.content, cell.id, createBundle]);
 
   return (
     <Resizable direction='vertical'>
@@ -37,7 +39,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)} 
           />
         </Resizable>
-        <Preview code={code} err={err} />
+        {bundle && <Preview code={bundle.code} err={bundle.err} />}
       </div>
     </Resizable>
   )
--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,6 +1,15 @@
+import { Dispatch } from 'redux';
 import { ActionType } from '../action-types';
-import { Action, Direction, UpdateCellAction, DeleteCellAction, MoveCellAction, InsertCellAfterAction } from '../actions';
+import { 
+    Action,
+    Direction, 
+    UpdateCellAction, 
+    DeleteCellAction, 
+    MoveCellAction, 
+    InsertCellAfterAction,
+} from '../actions';
 import { CellTypes } from '../cell';
+import bundle from '../../bundler';
 
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
@@ -38,4 +47,25 @@ export const insertCellAfter = (id: string | null, cellType: CellTypes): InsertC
             type: cellType,
         }
     }
+};
+
+export const createBundle = (cellId: string, input: string) => {
+    return async (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.BUNDLE_START,
+            payload: {
+                cellId,
+            }
+        });
+
+        const result = await bundle(input);
+
+        dispatch({
+            type: ActionType.BUNDLE_COMPLETE,
+            payload: {
+                cellId,
+                bundle: result,
+            }
+        });
+    };
 };
